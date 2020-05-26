2ad46c61093f1f76cd4b06e3d6f3498cd0db66e7
Daniel Fischer <dfischer17htl@outlook.com>
2020-05-26 19:15:25 +0200

    added comment

--- app/src/main/java/com/example/alex/MainActivity.java
+++ app/src/main/java/com/example/alex/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Kommentar
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
